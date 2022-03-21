375ed2bacd25f98c786cc40794a3ddf817a8eeab
EmmanuelKumah <mailkumah@gmail.com>
2022-03-21 21:41:54 +0000

    Style Navigation bar

--- src/Components/Layouts/Navigation/NavBar.js
+++ src/Components/Layouts/Navigation/NavBar.js
@@ -1,23 +1,26 @@
 import React from "react";
-import logo from "./food-logo.png";
+import logo from "./foodLogo.png";
 import classes from "./NavBar.module.css";
 import { FaShoppingCart, FaSearch } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 function NavBar() {
   return (
-    <nav className="navbar-container">
-      <div className="logo-container">
+    <nav className={classes.navbar}>
+      <div className={classes.logo}>
         <img src={logo} alt="food-logo" />
       </div>
-      <ul className="nav-menu">
-        <li className="menu-item">Home</li>
-        <li className="menu-item">About</li>
-        <li className="menu-item">Services</li>
-        <li className="menu-item">Menu</li>
+      <ul className={classes.nav__menu}>
+        <li className={classes.menu__item}>Home</li>
+        <li className={classes.menu__item}>About</li>
+        <li className={classes.menu__item}>Services</li>
+        <li className={classes.menu__item}>Menu</li>
       </ul>
       <div>
-        <FaShoppingCart />
-        <FaSearch />
+        <IconContext.Provider value={{ color: "white" }}>
+          <FaShoppingCart className={classes.nav__icons} />
+          <FaSearch className={classes.nav__icons} />
+        </IconContext.Provider>
       </div>
     </nav>
   );
